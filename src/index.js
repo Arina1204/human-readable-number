9be4d1050faa5709b83fedb996aa5f4a89b640e7
Arina1204 <arinachirets@gmail.com>
2026-05-09 21:25:57 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  if (number < 20) {
+    return ones[number];
+  }
+  if (number < 100) {
+    const ten = Math.floor(number / 10);
+    const one = number % 10;
+    if (one === 0) {
+      return tens[ten];
+    }
+    return `${tens[ten]} ${ones[one]}`;
+  }
+
+  const hundred = Math.floor(number / 100);
+  const rest = number % 100;
+  if (rest === 0) {
+    return `${ones[hundred]} hundred`;
+  }
+  return `${ones[hundred]} hundred ${toReadable(rest)}`;
 };
